3e33884f026147276499ff55c31510f2818f2039
Romain Goulet <romain.goulet@devinci.fr>
2019-11-17 09:14:31 +0100

    Liste client

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,28 @@ class Index extends React.Component {
           onCancel={() => this.setState({ open: false })}
         />
         <Layout>
+        <Query query={GET_ALL_CUSTOMERS}>
+          {({ data, loading, error }) => {
+            if (loading) return <div>Loading…</div>;
+            if (error) return <div>{error.message}</div>;
+            console.log(data);
+            return (
+                <div>
+                {data.customers.edges.map((edges, index)=>(
+                  <div>
+                  <ul>
+                          <li> CUSTOMERS N°{index}</li>
+                          <li> ID :  {edges.node.id}</li>
+                          <li> NAME : {edges.node.firstName} {edges.node.lastName}</li>
+                          <li> EMAIL : {edges.node.email}</li>
+                          <li> PHONE : {edges.node.phone}</li>
+                      </ul>
+                  </div>
+                 ))}
+                </div>
+            );
+          }}
+        </Query>
           <EmptyState
             heading="Select products to start"
             action={{
@@ -53,27 +75,7 @@ class Index extends React.Component {
             <p>Select products and change their price temporarily</p>
           </EmptyState>
         </Layout>
-        <Query query={GET_ALL_CUSTOMERS}>
-          {({ data, loading, error }) => {
-            if (loading) return <div>Loading…</div>;
-            if (error) return <div>{error.message}</div>;
-            console.log(data);
-            return (
-                <div>
-                {data.customers.edges.map((edges, index)=>(
-                  <div>
-                  <ul>
-                          <li> {edges.node.id}</li>
-                          <li> {edges.node.firstName} {edges.node.lastName}</li>
-                          <li> {edges.node.email}</li>
-                          <li> {edges.node.phone}</li>
-                      </ul>
-                  </div>
-                 ))}
-                </div>
-            );
-          }}
-        </Query>
+      
 
       </Page >
     );
